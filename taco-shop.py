36ff2c8751646324c3bb036108b671122983e68c
Shubhendu Shekhar <9253059+shekhar-shubhendu@users.noreply.github.com>
2021-06-29 11:41:14 +0200

    Update taco-shop.py

--- taco-shop.py
+++ taco-shop.py
@@ -15,6 +15,6 @@ class TacoShop(sp.Contract):
     def buy_taco(self, taco_type):
         taco = self.data.storage[taco_type]
         current_price = taco["max_price"] / taco["current_stock"]
-        sp.verify(sp.amount != current_price, "Sorry, the taco you are trying to purchase has a different price")
-        sp.verify(taco["current_stock"] == 0, "Sorry, no more tacos")
+        sp.verify(sp.amount == current_price, "Sorry, the taco you are trying to purchase has a different price")
+        sp.verify(taco["current_stock"] > 0, "Sorry, no more tacos")
         self.data.storage[taco_type]["current_stock"] = taco["current_stock"] - 1
